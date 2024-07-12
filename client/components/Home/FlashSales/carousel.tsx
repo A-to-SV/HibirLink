e3b042f2858a0a62094be4818b191270d92d309e
Fraol7 <fraolmulugeta17@gmail.com>
2024-07-12 14:50:11 +0300

    Improve responsiveness

--- client/components/Home/FlashSales/carousel.tsx
+++ client/components/Home/FlashSales/carousel.tsx
@@ -12,14 +12,14 @@ import { ProductsList } from "./products"
 
 export function CarouselSpacing() {
   return (
-    <Carousel className="flex flex-col mx-auto m-12 relative"> 
+    <Carousel className="flex flex-col mx-auto m-4 md:m-12 relative"> 
     <div className="absolute top-0 right-0 flex justify-between items-center z-10 mr-12">
         <CarouselPrevious className="bg-gray-200"/>
         <CarouselNext />
     </div>
     <CarouselContent className="ml-1 mt-8">
         {ProductsList.map((product: { percent: string; image: string; imageAlt: string; name: string; price: string; oldPrice: string; rating: string; vote: string }, index: React.Key | null | undefined) => (
-        <CarouselItem key={index} className="pl-5 basis-1/2 md:basis-1/3 lg:basis-1/4"> 
+        <CarouselItem key={index} className="pl-4 basis-1/2 md:basis-1/3 lg:basis-1/4"> 
             <ProductCard
                 key={index}
                 percent={product.percent}
