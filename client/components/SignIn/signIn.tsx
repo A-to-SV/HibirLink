8fa8b891f7aa7cec1e2f0af2b0a8c4e53954ff5d
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2024-07-14 12:16:42 +0300

    edit the folder structure of login and signup

--- client/components/SignIn/signIn.tsx
+++ client/components/SignIn/signIn.tsx
@@ -6,7 +6,7 @@ import { MdVisibility } from "react-icons/md";
 import { MdVisibilityOff } from "react-icons/md";
 import Image from 'next/image';
 
-const SignUp = () => {
+const SignIn = () => {
     const [showPassword, setShowPassword] = useState<boolean>(false);
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -22,30 +22,27 @@ const SignUp = () => {
                             width={56}
                             height={56}
                             className="w-10 h-10 md:w-12 md:h-12 lg:w-14 lg:h-14"
-                        />
-                        <span>
+                        />                        <span>
                             <u>HibrLink</u>
                             <p className="text-xs md:text-sm lg:text-sm font-light mt-1 text-gray-500">Connecting You to Everything, Everywhere</p>
                         </span>
                     </h1>
                 </div>
                 <section className="mt-10 md:mt-15 lg:mt-20 hidden md:block">
-                    <Image
-                        src="/assets/cart.png"
-                        alt="Cart Image"
-                        width={200}
-                        height={200}
-                        className='w-60 h-60 lg:w-96 lg:h-96'
-                    />
+                    <h2 className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-blue-600">
+                        Connecting You to Everything, Everywhere
+                    </h2>
+                    <p className="mt-2 md:mt-3 lg:mt-4 text-sm md:text-md lg:text-md text-gray-700">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
                 </section>
             </div>
             <div style={{ maxWidth: '500px' }} className="bg-white lg:ml-60 mt-16 p-10 rounded-3xl shadow-lg w-full lg:mr-20">
                 <div className="text-[#084E9A] text-start">
-                    <p className="mb-2 text-sm">Let&apos;s get you started</p>
-                    <h3 className="text-lg font-bold text-gray-600 mb-4">Create an account</h3>
+                    <p className="mb-2 text-sm">Welcome back!</p>
+                    <h3 className="text-lg font-bold text-gray-600 mb-4">Sign in</h3>
                 </div>
-                <form className=''>
-                    <TextField type='text' placeholder='John Doe' className='w-full mb-4' id="name" label="Name" variant="outlined" />
+                <form>
                     <TextField type='email' placeholder='johnsondoe@nomail.com' className='w-full mb-4' id="email" label="Email" variant="outlined" />
                     <FormControl className='w-full mb-10' sx={{ width: '25ch' }} variant="outlined">
                         <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
@@ -71,7 +68,7 @@ const SignUp = () => {
                             type="submit"
                             className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200"
                         >
-                            GET STARTED
+                            Sign in
                         </button>
                     </div>
                     <div className='flex justify-center items-center mt-8 mb-4'>
@@ -82,7 +79,7 @@ const SignUp = () => {
                     <div className="mb-1">
                         <button
                             type="button"
-                            className="w-full p-2 border border-gray-300 rounded-lg flex items-center justify-center relative hover:bg-gray-100 transition duration-200"
+                            className="w-full p-2 border border-gray-300 rounded flex items-center justify-center relative hover:bg-gray-100 transition duration-200"
                         >
                             <Image src="/assets/google.png" alt="Google Logo" width={16} height={16} className="absolute left-8" />
                             <span className="flex-grow text-center">Sign in with Google</span>
@@ -91,7 +88,7 @@ const SignUp = () => {
                     <div className="mb-1">
                         <button
                             type="button"
-                            className="w-full p-2 border border-gray-300 rounded-lg flex items-center justify-center relative hover:bg-gray-100 transition duration-200"
+                            className="w-full p-2 border border-gray-300 rounded flex items-center justify-center relative hover:bg-gray-100 transition duration-200"
                         >
                             <Image src="/assets/facebook.png" alt="Facebook Logo" width={16} height={16} className="absolute left-8" />
                             <span className="flex-grow text-center">Sign in with Facebook</span>
@@ -100,7 +97,7 @@ const SignUp = () => {
                     <div>
                         <button
                             type="button"
-                            className="w-full p-2 border border-gray-300 rounded-lg flex items-center justify-center relative hover:bg-gray-100 transition duration-200"
+                            className="w-full p-2 border border-gray-300 rounded flex items-center justify-center relative hover:bg-gray-100 transition duration-200"
                         >
                             <Image src="/assets/apple.png" alt="Apple Logo" width={16} height={16} className="absolute left-8" />
                             <span className="flex-grow text-center">Sign in with Apple</span>
@@ -115,4 +112,4 @@ const SignUp = () => {
     );
 }
 
-export default SignUp;
+export default SignIn;
