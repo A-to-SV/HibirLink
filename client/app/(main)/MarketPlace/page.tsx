8093ca7721895922849ebcf9cff41d3ad63ebad8
Fraol7 <fraolmulugeta17@gmail.com>
2024-07-15 11:46:47 +0300

    updating card

--- client/app/(main)/MarketPlace/page.tsx
+++ client/app/(main)/MarketPlace/page.tsx
@@ -1,12 +1,20 @@
+import React from 'react'
 import JustForYouSection from '@/components/MarketPlace/justForYouSection'
 import OurProductsSection from '@/components/MarketPlace/productsSection'
-import React from 'react'
+import ProductPage from '@/components/MarketPlace/sellingAsBusiness'
+import ReadyToShipProducts from '@/components/MarketPlace/readyToShipProducts'
+import CustomizableProduct from '@/components/MarketPlace/customizableProducts'
+import ProductGrid from '@/components/MarketPlace/productGrid'
+import Footer from '@/components/MarketPlace/footer'
 
 const page = () => {
     return (
         <div>
             <JustForYouSection />
             <OurProductsSection />
+            <ProductPage />
+            <ProductGrid />
+            <Footer />
         </div>
     )
 }
