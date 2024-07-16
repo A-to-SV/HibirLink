fa6d782f8ecc88dff4a1a0b3c1b2f79e2de68e6c
Fraol7 <fraolmulugeta17@gmail.com>
2024-07-16 16:55:38 +0300

    Error page update

--- client/app/error.tsx
+++ client/app/error.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
   return (
     <div className='h-screen w-full flex justify-center items-center'>
         <div className='space-y-3 text-center'>
-          <h1 className='text-9xl font-extrabold text-center'>404</h1>
+          <img src='/assets/404.png' alt='404' className='mb-4'/>
           <p className='text-xl'>page not found</p>
           <button className='rounded-lg text-white text-center p-4 bg-blue-800' onClick={() => router.push("/home") } >Back to Home page</button>
       </div>
