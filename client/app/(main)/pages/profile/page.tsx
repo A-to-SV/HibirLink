b15574b88690d44979a02d40cc4eb8695266f088
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-07-16 16:24:23 +0300

    finished the basic skeleton

--- client/app/(main)/pages/profile/page.tsx
+++ client/app/(main)/pages/profile/page.tsx
@@ -8,7 +8,7 @@ const ProfilePage = () => {
     gender: '',
     language: '',
     country: '',
-    tellno: '',
+    telno: '',
     role: '',
   });
 
@@ -44,11 +44,13 @@ const ProfilePage = () => {
     <div >
       <div className='bg-gradient-to-r from-light-blue-500 to-white mt-20 h-16'></div>
       <div>
-        <img src="" alt="" />
+        <div>
+          <img src="" alt="" />
+        </div>
       </div>
       <form onSubmit={handleSubmit} className=" p-4  flex items-center justify-center">
-        <div className="container mx-auto bg-white rounded border p-20">
-          <div className="flex flex-col md:flex-row lg:flex-row items-center justify-center">
+        <div className="container mx-auto bg-white rounded border-2 p-20 ml-10 mr-10">
+          <div className="flex flex-col md:flex-row lg:flex-row  justify-center">
             <div className="md:w-1/2 lg:w-1/2 flex flex-col space-y-4 m-5">
               <label htmlFor="fullname">Full Name</label>
               <input
@@ -57,7 +59,7 @@ const ProfilePage = () => {
                 placeholder="Full Name"
                 value={formData.fullname}
                 onChange={handleChange}
-                className="input-field border rounded"
+                className="input-field border-2 rounded h-10"
               />
               <label htmlFor="gender">Gender</label>
               <input
@@ -66,7 +68,7 @@ const ProfilePage = () => {
                 placeholder="Gender"
                 value={formData.gender}
                 onChange={handleChange}
-                className="input-field border rounded"
+                className="input-field border-2 rounded h-10"
               />
               <label htmlFor="language">Language</label>
               <input
@@ -75,43 +77,48 @@ const ProfilePage = () => {
                 placeholder="Language"
                 value={formData.language}
                 onChange={handleChange}
-                className="input-field"
+                className="input-field border-2 rounded h-10"
               />
               <div className='flex flex-col'>
-                <p>My email address</p>
+                <p>My Email Address</p>
                 <div>
                   <input type="radio" name="email" />
-                  <span>yonatan.tizazu@a2sv.org</span>
+                  <span className='m-3'>yonatan.tizazu@a2sv.org</span>
                 </div>
                 <div>
                   <input type="radio" name="email" />
-                  <span>melakeselam.yitbarek@a2sv.org</span>
+                  <span className='m-3'>melakeselam.yitbarek@a2sv.org</span>
                 </div>
                 
-                <input type="email" name="new-email"/>
-                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-28'>Add Email</button>
+                <input 
+                  type="email" 
+                  name="new-email"
+                  placeholder="New Email"
+                  className='border-2 rounded h-10 mt-4'
+                  />
+                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-28 m-5 ml-0'>Add Email</button>
               </div>
             </div>
-            <div className="md:w-1/2 lg:w-1/2 flex flex-col space-y-4">
+            <div className="md:w-1/2 lg:w-1/2 flex flex-col space-y-4 m-5">
               {/* <div> */}
                 <label htmlFor="country">Country</label>
                 <select
                   name="country"
                   value={formData.country}
                   onChange={handleChange}
-                  className="select-field"
+                  className="select-field border-2 rounded h-10"
                 >
                   <option value="">Select Country</option>
                   {/* Add options here */}
                 </select>
-                <label htmlFor="tellno">Tell No</label>
+                <label htmlFor="telno">Tel No</label>
                 <input
                   type="text"
-                  name="tellno"
-                  placeholder="Tell No"
-                  value={formData.tellno}
+                  name="telno"
+                  placeholder="Tel No"
+                  value={formData.telno}
                   onChange={handleChange}
-                  className="input-field"
+                  className="input-field border-2 rounded h-10"
                 />
                 <p>Please Select Role</p>
                 <div className='flex flex-row'>
@@ -123,7 +130,7 @@ const ProfilePage = () => {
                       onChange={handleChange}
                       checked={formData.role === 'user'}
                     />
-                    <span className="ml-2">User</span>
+                    <span className="ml-2 mr-4">User</span>
                   </label>
                   <label className="inline-flex items-center">
                     <input
@@ -133,7 +140,7 @@ const ProfilePage = () => {
                       onChange={handleChange}
                       checked={formData.role === 'ServiceProvider'}
                     />
-                    <span className="ml-2">Service Provider</span>
+                    <span className="ml-2 mr-4">Service Provider</span>
                   </label>
                   <label className="inline-flex items-center">
                     <input
@@ -143,15 +150,15 @@ const ProfilePage = () => {
                       onChange={handleChange}
                       checked={formData.role === 'deliveryPersonnel'}
                     />
-                    <span className="ml-2">Delivery Personnel</span>
+                    <span className="ml-2 mr-4">Delivery Personnel</span>
                   </label>
                 </div>
 
                 
               {/* </div> */}
-              <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-28">
+              {/* <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-28">
                 Submit
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
