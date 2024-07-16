84cbb635d8746426d7fc73c0112ff1d2ce1e4b74
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2024-07-16 16:34:06 +0300

    add navbar in for the serice page

--- client/app/(main)/Services/page.tsx
+++ client/app/(main)/Services/page.tsx
@@ -1,11 +1,13 @@
 import BusinessBanner from '@/components/Services/doYouOfferServices'
 import ServiceCard from '@/components/Services/serviceCard'
 import Footer from '@/components/MarketPlace/footer'
-import React from 'react'
+import React from 'react';
+import CommonNavBar from '@/components/CommonNavBar';
 
 const page = () => {
   return (
     <div>
+      <CommonNavBar />
       <BusinessBanner />
       <ServiceCard />
       <Footer />
