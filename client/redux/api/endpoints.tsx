8ed2100f288ab44ed3a3d8dedba2b06ad78bda9a
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2024-07-18 14:40:06 +0300

    remove user id from endpoint

--- client/redux/api/endpoints.tsx
+++ client/redux/api/endpoints.tsx
@@ -42,7 +42,6 @@ export const endpointsApi = createApi({
         url: '/api/products',
         method: 'POST',
         body: {
-          user_id: 1, // Ensure user_id is correct or obtained dynamically
           name,
           price,
           image,
